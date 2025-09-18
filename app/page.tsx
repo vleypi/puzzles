a6149d5d6c771ac40e35b1810c8f3f305ec97d3a
vleypi <vleypi@gmail.com>
2025-09-18 13:43:01 +0300

    Update Tool component links on HomePage to reflect new navigation for statistics and achievements

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,10 @@ export default function HomePage() {
           <Tool href="library" title='Библиотека' description='Библиотека ваших наборов' color='from-emerald-700 to-emerald-800 shadow-emerald-700/25'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-book-open w-6 h-6"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
           </Tool>
-          <Tool href="create-set" title='Тест' description='Проверить знания' color='from-purple-600 to-purple-700 shadow-purple-600/25'>
+          <Tool href="statistics" title='Статистика' description='Узнать свою статистику' color='from-purple-600 to-purple-700 shadow-purple-600/25'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-brain w-6 h-6"><path d="M12 5a3 3 0 1 0-5.997.125 4 4 0 0 0-2.526 5.77 4 4 0 0 0 .556 6.588A4 4 0 1 0 12 18Z"></path><path d="M12 5a3 3 0 1 1 5.997.125 4 4 0 0 1 2.526 5.77 4 4 0 0 1-.556 6.588A4 4 0 1 1 12 18Z"></path><path d="M15 13a4.5 4.5 0 0 1-3-4 4.5 4.5 0 0 1-3 4"></path><path d="M17.599 6.5a3 3 0 0 0 .399-1.375"></path><path d="M6.003 5.125A3 3 0 0 0 6.401 6.5"></path><path d="M3.477 10.896a4 4 0 0 1 .585-.396"></path><path d="M19.938 10.5a4 4 0 0 1 .585.396"></path><path d="M6 18a4 4 0 0 1-1.967-.516"></path><path d="M19.967 17.484A4 4 0 0 1 18 18"></path></svg>
           </Tool>
-          <Tool href="create-set" title='Цели' description='Установить цели' color='from-orange-600 to-orange-700 shadow-orange-600/25'>
+          <Tool href="achievements" title='Достижения' description='Ваши достижения' color='from-orange-600 to-orange-700 shadow-orange-600/25'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-target w-6 h-6"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="6"></circle><circle cx="12" cy="12" r="2"></circle></svg>
           </Tool>
         </section>
